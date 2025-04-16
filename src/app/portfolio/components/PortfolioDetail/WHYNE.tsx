5a64baad3eb6d30c6907cfb20e61214415177f2b
HyeyeonJung <jhy20306@gmail.com>
2025-04-16 21:26:06 +0900

    chore: portfolio modal mediaquery

--- src/app/portfolio/components/PortfolioDetail/WHYNE.tsx
+++ src/app/portfolio/components/PortfolioDetail/WHYNE.tsx
@@ -271,13 +271,15 @@ export default function Whyne() {
           href="https://github.com/yujuseop/Project-Team2-WINE"
           target="_blank"
         >
-          <Image
-            className={styles.overviewImg}
-            src="/logo/github.png"
-            alt="github repository"
-            width={150}
-            height={150}
-          />
+          <div className={styles.githubLogo}>
+            <Image
+              className={styles.githubLogo}
+              src="/logo/github.png"
+              alt="github repository"
+              width={150}
+              height={150}
+            />
+          </div>
         </Link>
       </div>
     </div>
